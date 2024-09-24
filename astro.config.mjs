dba4bd0410350fbff73ceb2b9fab43df622e1edb
Harry Lang <harrylang100@gmail.com>
2024-09-24 21:27:24 +0100

    Update config
    Change site domain name from http to https

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,6 @@ import prefetch from "@astrojs/prefetch";
 // https://astro.build/config
 export default defineConfig({
   // TODO: Change this in production
-  site: "http://harrylang100.github.io",
+  site: "https://harrylang100.github.io",
   integrations: [tailwind(), sitemap(), prefetch({ throttle: 3 })],
 });
